177d932ca0db57374f712794f94180e32753f22a
danielprinced <d.danielprince2003@gmail.com>
2026-05-27 22:50:40 +0530

    lambda example

---
+++ src/main/java/com/practice/functional/LambdaExample.java
@@ -0,0 +1,13 @@
+package com.practice.functional;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class LambdaExample
+{
+
+	public static void main(String[] args)
+	{
+
+	}
+}
