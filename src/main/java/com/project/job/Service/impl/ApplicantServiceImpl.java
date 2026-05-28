33d376843a513e71a6496144c8f936867cf934b5
danielprinced <d.danielprince2003@gmail.com>
2026-05-28 23:32:14 +0530

    applicant service implementation

--- src/main/java/com/project/job/Service/impl/ApplicantServiceImpl.java
+++ src/main/java/com/project/job/Service/impl/ApplicantServiceImpl.java
@@ -1,9 +1,54 @@
 package com.project.job.Service.impl;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class ApplicantServiceImpl
-{
+import com.project.job.Repository.ApplicantRepository;
+import com.project.job.Service.ApplicantService;
+import com.project.job.dto.mapper.ApplicantMapper;
+import com.project.job.dto.request.ApplicantRequest;
+import com.project.job.dto.response.ApplicantResponse;
+import com.project.job.pojo.ApplicantTable;
 
+@Service
+public class ApplicantServiceImpl implements ApplicantService
+{
+	@Autowired private ApplicantRepository applicantRepository;
+
+	@Override public ApplicantResponse getApplicantById(Long applicantId)
+	{
+		Optional<ApplicantTable> applicantTable = applicantRepository.findById(applicantId);
+		return applicantTable.map(ApplicantMapper::toApplicantResponse).orElse(null);
+	}
+
+	@Override public List<ApplicantResponse> getAllApplicants()
+	{
+		return applicantRepository.findAll().stream().map(ApplicantMapper::toApplicantResponse).toList();
+	}
+
+	@Override public ApplicantResponse addApplicant(ApplicantRequest applicant)
+	{
+		ApplicantTable applicantTable = applicantRepository.save(ApplicantMapper.toApplicantTable(applicant));
+		return ApplicantMapper.toApplicantResponse(applicantTable);
+	}
+
+	@Override public ApplicantResponse updateApplicant(Long applicantId, ApplicantRequest applicant)
+	{
+		ApplicantTable applicantTable = ApplicantMapper.toApplicantTable(applicant);
+		applicantTable.setApplicantId(applicantId);
+		applicantTable = applicantRepository.save(applicantTable);
+		return ApplicantMapper.toApplicantResponse(applicantTable);
+	}
+
+	@Override public boolean deleteApplicant(Long applicantId)
+	{
+		if (applicantRepository.existsById(applicantId)) {
+			applicantRepository.deleteById(applicantId);
+			return true;
+		}
+		return false;
+	}
 }
