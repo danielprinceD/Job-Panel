4b7109cf9bd81523f7e6aaea9354471c4d79d513
danielprinced <d.danielprince2003@gmail.com>
2026-05-28 21:25:04 +0530

    lambda function example

--- src/main/java/com/practice/functional/LambdaExample.java
+++ src/main/java/com/practice/functional/LambdaExample.java
@@ -8,6 +8,10 @@ public class LambdaExample
 
 	public static void main(String[] args)
 	{
+		Function<String, String> toUpperCase = str -> str.toUpperCase();
+		System.out.println(toUpperCase.apply("hello"));
 
+		Consumer<String> printWithPrefix = str -> System.out.println("Prefix: " + str);
+		printWithPrefix.accept("Hello");
 	}
 }
