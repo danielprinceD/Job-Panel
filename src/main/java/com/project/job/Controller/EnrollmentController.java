680626466de38fd1be2c3a8b70a9ed04ca5da51f
danielprinced <d.danielprince2003@gmail.com>
2026-05-30 16:47:51 +0530

    entrollment controller implemented

--- src/main/java/com/project/job/Controller/EnrollmentController.java
+++ src/main/java/com/project/job/Controller/EnrollmentController.java
@@ -1,12 +1,59 @@
 package com.project.job.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponseException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project.job.Service.EntrollmentService;
+import com.project.job.dto.response.EnrollmentResponse;
 
 @RequestMapping("/api/v1")
 @RestController
 public class EnrollmentController
 {
+	@Autowired private EntrollmentService entrollmentService;
 
+
+	@PostMapping("/enrolls")
+	public ResponseEntity<?> enrollApplicantToJob(@RequestParam("applicant_id") Long applicantId ,@RequestParam("job_id") Long jobId)
+	{
+		try {
+			EnrollmentResponse enrollmentResponse = entrollmentService.enrollApplicantToJob(applicantId , jobId);
+			return ResponseEntity.ok(
+				enrollmentResponse
+			);
+		}
+		catch(ErrorResponseException e){
+			return ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
+		}
+		catch(Exception e){
+			return ResponseEntity.status(500).body("An unexpected error occurred: " + e.getMessage());
+		}
+	}
+
+	@GetMapping("/entrolls/{enrollment_id}")
+	public ResponseEntity<?> getEnrollmentById( @PathVariable("enrollment_id") Long enrollmentId)
+	{
+		try {
+			EnrollmentResponse enrollmentResponse = entrollmentService.getEnrollmentById(enrollmentId);
+			if(enrollmentResponse == null){
+				return ResponseEntity.status(404).body("Enrollment with id " + enrollmentId + " not found.");
+			}
+			return ResponseEntity.ok(
+				enrollmentResponse
+			);
+		}
+		catch(ErrorResponseException e){
+			return ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
+		}
+		catch(Exception e){
+			return ResponseEntity.status(500).body("An unexpected error occurred: " + e.getMessage());
+		}
+	}
 }
