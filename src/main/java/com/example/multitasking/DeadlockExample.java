221f959187ca648dd278d4cda4d5d3577ebb728d
danielprinced <d.danielprince2003@gmail.com>
2026-06-12 08:20:37 +0530

    deadlock

---
+++ src/main/java/com/example/multitasking/DeadlockExample.java
@@ -0,0 +1,34 @@
+package com.example.multitasking;
+
+public class DeadlockExample
+{
+	public static void main(String[] args)
+	{
+		Object lock1 = new Object();
+		Object lock2 = new Object();
+
+		Thread th1 = new Thread(()->{
+			synchronized(lock1){
+				System.out.println("Thread 1: Holding lock 1...");
+				synchronized(lock2){
+					System.out.println("Thread 1: Holding lock 2...");
+				}
+			}
+		});
+
+		Thread th2 = new Thread(()->{
+			synchronized(lock2){
+				System.out.println("Thread 2: Holding lock 2...");
+				synchronized(lock1){
+					System.out.println("Thread 2: Holding lock 1...");
+				}
+			}
+		});
+
+		th1.start();
+		th2.start();
+
+
+
+	}
+}
