8be5aeddfb68e53fcdbe212a8f6450efc3b18239
danielprinced <d.danielprince2003@gmail.com>
2026-06-15 21:38:40 +0530

    Template design pattern with gameloader example

---
+++ src/main/java/com/example/designpattern/behavioral/TemplateDesignPattern.java
@@ -0,0 +1,63 @@
+package com.example.designpattern.behavioral;
+
+abstract class BaseGameLoader {
+
+	public abstract void loadAssets();
+	public abstract void initializeGame();
+	public void startGame() {
+		System.out.println("Default Game Started");
+	}
+
+	public void loadGame(){
+		loadAssets();
+		initializeGame();
+		startGame();
+	}
+}
+
+class GodOfWarGameLoader extends BaseGameLoader {
+	@Override public void loadAssets()
+	{
+		System.out.println("Loading God of War Assets");
+	}
+
+	@Override public void initializeGame()
+	{
+		System.out.println("Initializing God of War Game");
+	}
+
+	@Override public void startGame()
+	{
+		System.out.println("Starting God of War Game");
+	}
+}
+
+class SpiderManGameLoader extends BaseGameLoader
+{
+	@Override public void loadAssets()
+	{
+		System.out.println("Loading Spider-Man Game");
+	}
+
+	@Override public void initializeGame()
+	{
+		System.out.println("Initializing Spider-Man Game");
+	}
+
+}
+
+
+
+public class TemplateDesignPattern
+{
+	public static void main(String[] args)
+	{
+		BaseGameLoader gameLoader = new GodOfWarGameLoader();
+		gameLoader.loadGame();
+
+		System.out.println();
+
+		gameLoader = new SpiderManGameLoader();
+		gameLoader.loadGame();
+	}
+}
